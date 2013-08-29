3727d9abf6a16b02cacbbf04c1b8d114c938d015
Romeu Fonseca <romeu.hcf@gmail.com>
2013-08-29 09:21:05 -0300

    Testing running ok, validating bill without file, show phone_bill at ticket report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@
 require 'simplecov'
 SimpleCov.start
 require 'steroid_aggregations'
-require 'pry-debugger'
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
